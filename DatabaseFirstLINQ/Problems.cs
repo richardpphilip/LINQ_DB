b6cd9fc75f86ef98366009d6031f296cb788606f
Eric Lars Lee <ericlarslee@gmail.com>
2021-05-13 11:41:52 -0500

    Rich completed #16 via remote access. We saw the changed product price in the database.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -28,9 +28,9 @@ namespace DatabaseFirstLINQ
             //ProblemEleven();
             //ProblemTwelve();
             //ProblemThirteen();
-            ProblemFourteen();
-            //ProblemFifteen();
-            //ProblemSixteen();
+            //ProblemFourteen();
+            ProblemFifteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -244,6 +244,11 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
+            var product = _context.Products.Where(p => p.Id == 8).SingleOrDefault();
+            product.Price = 20;
+            _context.Products.Update(product);
+            _context.SaveChanges();
+                
 
         }
 
